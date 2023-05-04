e7b1291086da8552fb7fda13bb2c9af9a287997b
phamsang6260@gmail.com <phamsang6260@gmail.com>
2023-05-04 13:56:49 +0700

    feat

--- src/pages/stream.tsx
+++ src/pages/stream.tsx
@@ -4,7 +4,7 @@ import WebRTCPlayer from "~/components/WebRTCPlayer";
 
 const Stream: NextPage = () => {
   const webrtcUrl1 =
-    "http://localhost:8083/stream/55e904fd-0a0f-46d5-a78d-36094cfb65b8/channel/0/webrtc";
+    "http://localhost:8083/stream/d69e6af3-08d6-4197-a92a-8717119e5b32/channel/0/webrtc";
   return (
     <div className="h-screen w-screen bg-slate-500">
       <div className="flex gap-3">
